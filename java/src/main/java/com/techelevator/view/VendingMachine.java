fe23efb4051cc84dc922636fd45b58e2ed1371e0
Megan Levy <megan.levy3@gmail.com>
2021-10-09 18:44:08 -0400

    WIP

--- java/src/main/java/com/techelevator/view/VendingMachine.java
+++ java/src/main/java/com/techelevator/view/VendingMachine.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class VendingMachine {
@@ -74,7 +75,7 @@ public class VendingMachine {
     }
 
     //handle money
-    public BigDecimal feedMoney(BigDecimal moneyAdded) {
+    public void feedMoney(BigDecimal moneyAdded) {
         if (moneyAdded.equals(TWENTY)) {
             balance = balance.add(moneyAdded);
         } else if (moneyAdded.equals(TEN)) {
@@ -84,7 +85,8 @@ public class VendingMachine {
         } else if (moneyAdded.equals(DOLLAR)) {
             balance = balance.add(moneyAdded);
         }
-        return balance;
+        System.out.println("Your balance is: " + balance.setScale(2));
+        ;
     }
 
     public BigDecimal getBalance() {
@@ -93,15 +95,27 @@ public class VendingMachine {
     }
 
     public BigDecimal returnChange() {
+        /*
+        x = changereturned * 100 - all pennies
+        balance / 25 == quatersX
+        balance -= (quartersX * 25);
+
+
+        balance / 10 == dimesX;
+        balance -= (dimesX * 10);
+
+        balance / 5 == nicklesX;
+
+        print ("Your change is " + quartersX + " in quarters, " + dimesX + etc)
+
+
+         */
         return change;
     }
 
     public BigDecimal subtractBalance(BigDecimal itemCost) {
         balance = balance.subtract(itemCost);
         return balance;
-    }
-
-    public void getSelection() {
     }
 
 
@@ -118,10 +132,11 @@ public class VendingMachine {
         }
         System.out.println("Please select item by inputting slot location (ex. A1): ");
         String slotLocation = scanner.nextLine();
+        String formattedInput = slotLocation.toUpperCase();
 
         Product foundProduct = null;
         for (Product product : products) {
-            if (product.getSlotNumber().equals(slotLocation)) {
+            if (product.getSlotNumber().equals(formattedInput)) {
                 foundProduct = product;
             }
 
@@ -131,8 +146,14 @@ public class VendingMachine {
             return "Invalid Selection!";
         }
 
+//        if (foundProduct.getCost().compareTo(balance) < 0) {
+//
+//            return "Please insert more money";
+//        }
+// will come back to, make sure they cant take out more product than they can afford :(
 
         if (foundProduct.getInventoryCount() > 0) {
+
             subtractBalance(foundProduct.getCost());
             foundProduct.decrementInv();
             System.out.println(foundProduct.getItemName() + "|" + foundProduct.getCost() + "|" + getBalance() + "|" + foundProduct.getResponse());
@@ -143,6 +164,5 @@ public class VendingMachine {
         return "Successful purchase";
     }
 
-// add to cart, subtract from inventory count, subtract from balance
 }
 
